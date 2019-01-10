d1564a93b22758ea1e6078555afa31acdce33b92
Lachlan Lindsay <lindsaylachlan@gmail.com>
2019-01-10 19:58:14 +1000

    Initial Commit

---
+++ Scream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scream
+//
+//  Created by Lachlan Lindsay on 10/1/19.
+//  Copyright © 2019 Lachlan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
